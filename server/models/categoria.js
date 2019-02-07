6ecaf350292867bd0df1cf0464294f25e4c7c792
Allison <allison@rts.com.co>
2019-02-07 09:49:20 -0500

    Nuevos modelos y rutas.

---
+++ server/models/categoria.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+let Schema = mongoose.Schema;
+
+let categoriaSchema = new Schema({
+    descripcion: {
+        type: String,
+        unique: true,
+        required: [true, 'La descripción es obligatoria.']
+    }
+})
+
+categoriaSchema.plugin(uniqueValidator, { message: '{PATH} debe ser único.' })
+module.exports = mongoose.model('Categoria', categoriaSchema)
---
+++ server/routes/categoria.js
@@ -0,0 +1,134 @@
+const express = require('express');
+const Categoria = require('../models/categoria');
+const { verificaToken, verificaAdminRole } = require('../middlewares/autenticacion')
+
+const app = express();
+
+//Mostrar todas las categorias.
+app.get('/categoria', verificaToken, (req, res) => {
+
+    Categoria.find({})
+        //Revisa que ids existen en las ctegorias que estoy solicitando y permite 
+        //cargar información.
+        //.populate('usuario', 'nombre email')
+        .exec((err, categorias) => {
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    err
+                });
+            }
+
+            res.json({
+                ok: true,
+                categorias
+            });
+        })
+})
+
+//Mostrar una categoria por id.
+app.get('/categoria/:id', (req, res) => {
+    let id = req.params.id;
+
+    Categoria.findById(id, (err, categoriaDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            })
+        }
+        res.json({
+            ok: true,
+            categoria: categoriaDB
+        });
+    })
+})
+
+//Crear una nueva categoria.
+app.post('/categoria', verificaToken, (req, res) => {
+    let body = req.body
+
+    let categoria = new Categoria({
+        descripcion: body.descripcion,
+        usuario: req.usuario._id
+    })
+
+    categoria.save((err, categoriaDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!categoriaDB) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            categoria: categoriaDB
+        });
+    })
+})
+
+//Actualizar una categoria.
+app.put('/categoria/:id', (req, res) => {
+    let id = req.params.id;
+    let body = req.body;
+    let descCategoria = {
+        descripcion: body.descripcion
+    }
+
+    Categoria.findByIdAndUpdate(id, descCategoria, { new: true, runValidators: true, context: 'query' }, (err, categoriaDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!categoriaDB) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            categoria: categoriaDB
+        });
+    })
+})
+
+//Eliminar una categoria.
+app.delete('/categoria/:id', [verificaToken, verificaAdminRole], (req, res) => {
+    let id = req.params.id;
+
+    Categoria.findByIdAndRemove(id, (err, categoriaDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!categoriaDB) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            categoria: categoriaDB
+        });
+    })
+})
+
+module.exports = app;
